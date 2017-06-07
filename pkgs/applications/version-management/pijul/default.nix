60b1b85dc32b5d75b4c703cb4f6b59a1f7ee3bfd
Florent Becker <Florent.Becker@ens-lyon.org>
2017-06-07 10:24:44 +0200

    pijul: 0.4.4 -> 0.6.0

--- pkgs/applications/version-management/pijul/default.nix
+++ pkgs/applications/version-management/pijul/default.nix
@@ -4,21 +4,32 @@ with rustPlatform;
 
 buildRustPackage rec {
   name = "pijul-${version}";
-  version = "0.4.4";
+  version = "0.6.0";
 
-  src = fetchurl {
+  raw_src = fetchurl {
     url = "https://pijul.org/releases/${name}.tar.gz";
-    sha256 = "8f133b7e14bfa84156c103126d53b12c6dfb996dcdebcf1091199ff9c77f3713";
+    sha256 = "a6b066b49b25d1083320c5ab23941deee795e1fcbe1faa951e95189fd594cdb3";
   };
 
+  src = "${name}.tar.gz";
+
   sourceRoot = "${name}/pijul";
+
+  preUnpack = ''
+     tar -cf empty.tar -T /dev/null
+     cp ${raw_src} ${name}.tar.gz
+     chmod u+w ${name}.tar.gz
+     tar -xf ${name}.tar.gz --transform=s,^,${name}/,
+     tar -cf ${name}.tar.gz ${name}
+     rm -rf ${name}
+  '';
 
   buildInputs = [ perl ]++ stdenv.lib.optionals stdenv.isDarwin
     (with darwin.apple_sdk.frameworks; [ Security ]);
 
   doCheck = false;
-  
-  depsSha256 = "1zdvnarg182spgydmqwxxr929j44d771zkq7gyh152173i0xqb20";
+
+  depsSha256 = "0raim0ahqg6fkidb6picfzircdzwdbsdmmv8in70r5hw770bv67r";
 
   meta = with stdenv.lib; {
     description = "A distributed version control system";
